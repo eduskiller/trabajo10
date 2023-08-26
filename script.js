3964eb2905353e1cb2132ecf9d62fa4c58c63d6e
eduskiller <140122311+eduskiller@users.noreply.github.com>
2023-08-26 20:57:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var nombre = "Eduardo";
+var apellido ="Acevedo";
+console.log(nombre + " " + apellido);
+
+var numero1 = 1;
+var numero2 = 2;
+var suma = numero1 + numero2;
+ console.log("el resultado de la suma es " + suma)
+
+ var sumatotal = suma + 10;
+ console.log("el resultado de la suma total es " + sumatotal)
